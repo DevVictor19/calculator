046d6e88fff85ca7201cc7cacfe5ad57d39bc5f8
Antonio Victor <Antoniovictor12@live.com>
2022-11-27 20:29:39 -0300

    build: adicionar copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -61,5 +62,8 @@ module.exports = {
       template: "./src/template.html",
     }),
     new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: "src/public", to: "public" }],
+    }),
   ],
 };
