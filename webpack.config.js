5c2ca677021cbdbe9f6f93ce0a221347b882cc13
Antonio Victor <Antoniovictor12@live.com>
2022-11-27 18:33:06 -0300

    build: baixar mini-css plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "development",
@@ -27,7 +28,7 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.js$/,
@@ -47,5 +48,6 @@ module.exports = {
       filename: "index.html",
       template: "./src/template.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
 };
