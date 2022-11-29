0f505755972ae7cd95ec3d570b4de0f280717b12
Antonio Victor <Antoniovictor12@live.com>
2022-11-29 20:23:06 -0300

    refactor: implementar uso de classes

--- src/index.js
+++ src/index.js
@@ -1,50 +1,45 @@
-import "./styles/main.css";
+import "./styles.css";
+import { themes } from "./js/themes";
 
-import {
-  switchToNextTheme,
-  setTheme,
-  currentThemeIndex,
-} from "./js/themeSwitch";
+import ThemesController from "./js/themesController";
 
 import { handleBtnInputValue } from "./js/calculator";
 
-import {
-  playBtnSoundEffect,
-  playSwitchThemeSoundEffect,
-} from "./js/playSoundEffects";
-
+const themesController = new ThemesController(themes, "lastIndexTheme");
 const switcher = document.getElementById("switcher");
-
-// switcher click event
-switcher.addEventListener("click", () => {
-  switchToNextTheme();
-  setSwitcherBtnPositon(currentThemeIndex);
-  playSwitchThemeSoundEffect();
-  localStorage.setItem("lastIndexTheme", currentThemeIndex);
-});
-
-function setSwitcherBtnPositon(index) {
-  const switcherBtnPositions = {
-    0: "flex-start",
-    1: "center",
-    2: "flex-end",
-  };
-
-  switcher.style.justifyContent = switcherBtnPositions[index];
-}
-
-// load last theme on screen load
-window.addEventListener("load", () => {
-  setTheme(currentThemeIndex);
-  setSwitcherBtnPositon(currentThemeIndex);
-});
-
 const calculatorBody = document.getElementById("calculatorBody");
+
+const setSwitcherBtnPositon = (index) => {
+  let style = null;
+
+  switch (index) {
+    case 0:
+      style = "flex-start";
+      break;
+    case 1:
+      style = "center";
+      break;
+    case 2:
+      style = "flex-end";
+      break;
+  }
+
+  switcher.style.justifyContent = style;
+};
+
+switcher.addEventListener("click", () => {
+  themesController.switchToNextTheme();
+  themesController.saveLastTheme();
+  setSwitcherBtnPositon(themesController.currentThemeIndex);
+});
+
+window.addEventListener("load", () => {
+  themesController.setCurrentTheme();
+  setSwitcherBtnPositon(themesController.currentThemeIndex);
+});
 
 calculatorBody.addEventListener("click", (e) => {
   if (e.target === calculatorBody) return;
 
   handleBtnInputValue(e.target.innerText);
-
-  playBtnSoundEffect();
 });
