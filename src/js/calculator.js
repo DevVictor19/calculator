87dcd02190ac33e71cffb11ee212f11db24864b4
Antonio Victor <Antoniovictor12@live.com>
2022-11-30 08:27:10 -0300

    refactor(calculator): refatorar para class

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,101 +1,69 @@
-import { splitExpression } from "./utils/splitExpression";
-import { clearZerosBeforeDigits } from "./utils/clearZerosBeforeDigits";
+export class Calculator {
+  #displayElement;
 
-const displayText = document.getElementById("displayText");
-
-function appendOnDisplay(value) {
-  let maxLength = window.innerWidth > 540 ? 15 : 10;
-  const displayTextLength = displayText.innerHTML.length;
-
-  if (displayTextLength >= maxLength) return;
-
-  const expression = displayText.innerText + value;
-
-  displayText.innerText = splitExpression(expression)
-    .map(clearZerosBeforeDigits)
-    .join("");
-}
-
-function resetAllFromDisplay() {
-  displayText.innerText = "";
-}
-
-function deleteCharFromDisplay() {
-  displayText.innerText = displayText.innerText.substring(
-    0,
-    displayText.innerText.length - 1
-  );
-}
-
-function handleEquals() {
-  const expression = displayText.innerText.replace("x", "*");
-
-  if (/[\+\-\*\/]/.test(expression) && expression.length === 1) return;
-
-  const result = eval(expression);
-
-  if (result === Infinity) {
-    alert("Não é possível dividir por 0");
-    return;
+  constructor(displayElement) {
+    this.#displayElement = displayElement;
   }
 
-  displayText.innerText = result;
+  handleInput(value) {
+    switch (value) {
+      case "DEL":
+        this.#delete();
+        break;
+      case "RESET":
+        this.#reset();
+        break;
+      case "=":
+        this.#resolve();
+        break;
+      default:
+        this.#append(value);
+        break;
+    }
+  }
+
+  #insert(value) {
+    this.#displayElement.innerText = value;
+  }
+
+  #append(value) {
+    const updatedDisplay = this.#displayElement.innerText + value;
+    const maxLength = window.innerWidth > 540 ? 15 : 10;
+    const displayTextLength = updatedDisplay.length;
+
+    if (displayTextLength >= maxLength) return;
+
+    // validar zeros antes de numeros
+    // validar .
+    // validar operadores
+
+    this.#insert(updatedDisplay);
+  }
+
+  #reset() {
+    this.#insert("");
+  }
+
+  #delete() {
+    let str = this.#displayElement.innerText;
+    str = str.slice(0, str.length - 1);
+    this.#insert(str);
+  }
+
+  #resolve() {
+    const expression = this.#displayElement.innerText.replace("x", "*");
+
+    if (/[\+\-\*\/]/.test(expression) && expression.length === 1) return;
+
+    const result = eval(expression);
+
+    if (result === Infinity) {
+      alert("Não é possível dividir por 0");
+      return;
+    }
+
+    this.#insert(result);
+  }
 }
 
-function handleOperators(value) {
-  const updatedDisplayText = displayText.innerText + value;
-
-  const hasConsecutiveOperators = /[\+\-\x\/]{2,}/.test(updatedDisplayText);
-
-  if (hasConsecutiveOperators) return;
-
-  appendOnDisplay(value);
-}
-
-function handleDecimalDot(value) {
-  const updatedDisplayText = displayText.innerText + value;
-
-  const hasTwoDotsInSameNumber = /\d+\.\d+\./.test(updatedDisplayText);
-  const hasConsecutiveDots = /\.{2,}/.test(updatedDisplayText);
-  const startsWithDot = /\D\./.test(updatedDisplayText);
-  const isTheFirstChar = updatedDisplayText.length === 1;
-
-  if (
-    hasTwoDotsInSameNumber ||
-    hasConsecutiveDots ||
-    startsWithDot ||
-    isTheFirstChar
-  )
-    return;
-
-  appendOnDisplay(value);
-}
-
-export function handleBtnInputValue(value) {
-  if (value === "RESET") {
-    resetAllFromDisplay();
-    return;
-  }
-
-  if (value === "DEL") {
-    deleteCharFromDisplay();
-    return;
-  }
-
-  if (value === "=") {
-    handleEquals();
-    return;
-  }
-
-  if (value === "+" || value === "-" || value === "/" || value === "x") {
-    handleOperators(value);
-    return;
-  }
-
-  if (value === ".") {
-    handleDecimalDot(value);
-    return;
-  }
-
-  appendOnDisplay(value);
-}
+export default Calculator;
